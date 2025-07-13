03f8057465e4164ebbfd57847c777adffe8122df
Gayantha Maduwantha <134595232+GayanthaMadhuwantha@users.noreply.github.com>
2025-07-13 14:33:02 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -39,7 +39,7 @@ function App() {
               }
             />
             <Route path="/" element={<Navigate to="/dashboard" replace />} />
-            <Route path="*" element={<Navigate to="/dashboard" replace />} />
+            <Route path="*" element={<div >404 Not Found</div>} />
           </Routes>
         </div>
       </Router>
