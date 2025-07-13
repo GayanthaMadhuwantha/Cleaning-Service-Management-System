45dd3b8f1605b8f24126bda1cb3846dbd05bf25e
Gayantha Maduwantha <134595232+GayanthaMadhuwantha@users.noreply.github.com>
2025-07-13 16:48:20 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js';
+import bookingRoutes from './routes/bookings.js';
+import serviceRoutes from './routes/services.js';
+
+
+dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-
 const app=express()
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-import authRoutes from './routes/auth.js';
-import bookingRoutes from './routes/bookings.js';
-import serviceRoutes from './routes/services.js';
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/bookings', bookingRoutes);
